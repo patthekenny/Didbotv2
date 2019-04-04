7d3f9d4868a134f18dc8e079b30aeef76968a872
Patrick Kenny <patrick@didey.net>
2019-04-03 23:16:29 -0400

    Removes blackjack.

--- index.js
+++ index.js
@@ -5,11 +5,6 @@ is_playing_sound = false;
 
 const permitted_audio_formats = [".MP3", ".OGG", ".WAV", "FLAC", "MIDI", ".WMA", ".M4A"]
 
-let in_blackjack_game = false;
-let current_blackjack_player_id;
-let current_dealer_hand = 0;
-let current_player_hand = 0;
-
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
 }); 
@@ -24,55 +19,8 @@ client.on('message', msg => {
         client.voiceConnections.get(guild_snowflake).disconnect();
         msg.delete();
     }
-    // set timeout
-    // min card is 2
-    if(msg.content === ".blackjack") {
-        if(!in_blackjack_game) {
-            in_blackjack_game = true;
-            msg.reply(`Game starting now!`);
-            current_blackjack_player_id = msg.member.id;
-            current_dealer_hand += generate_blackjack_value();
-            current_player_hand += generate_blackjack_value();
-            msg.reply(`Your hand is currently a ${current_player_hand}, .hit or .stick?`);
-        } else {
-            msg.reply(`Already playing a blackjack game with ${current_blackjack_player_id}`);
-        }
-    }
-
-    if(msg.content === ".hit") {
-        if(msg.member.id != current_blackjack_player_id) return msg.reply("You're not the player currently in a game!");
-
-        
-        current_player_hand += generate_blackjack_value();
-        if(current_player_hand === 21) {
-            msg.reply("21! You win!");
-            return end_blackjack_game();
-        } 
-        if(current_player_hand > 21) {
-            msg.reply("Bust, https://i.imgur.com/RtteDyZ.jpg");
-            return end_blackjack_game();
-        }
-        
-        msg.reply(`Your hand is currently a ${current_player_hand}, .hit or .stick?`);
-
-    }
-
-    if(msg.content === ".stick") {
-        msg.reply("Only faggots don't hit. You lose!");
-        end_blackjack_game();
-    }
 
 });
-
-let end_blackjack_game = () => {
-    current_blackjack_player_id = null;
-    current_dealer_hand = 0;
-    current_player_hand = 0;
-};
-
-let generate_blackjack_value = () => {
-    return Math.floor(Math.random() * (Math.floor(10) - Math.ceil(2) + 1)) + Math.ceil(2);
-}
 
 let check_for_attachments = function(msg) {
     if(!msg.attachments.first()) return;
